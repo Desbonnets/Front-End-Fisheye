705a6a957c9e115916a04d05358f19d209db243a
Desbonnets <gillesdesbonnets@gmail.com>
2022-11-30 11:59:47 +0100

    Premiere pages

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,78 @@
-function photographerFactory(data) {
-    const { name, portrait } = data;
+function photographersFactory(data) {
+    const { name, portrait, city, country, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `assets/photographers/Photographers ID Photos/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
+        img.setAttribute("src", picture);
+        img.setAttribute("class", "portrait");
+        const a = document.createElement('a');
+        a.setAttribute("href", "./photographer.html?"+name)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        const p = document.createElement( 'p' );
+        const p1 = document.createElement( 'p' );
+        const p2 = document.createElement( 'p' );
+        p.textContent = city + ', ' + country;
+        p1.textContent = tagline;
+        p2.textContent = price +' €/ jour';
+        p.setAttribute("class", "lieu");
+        p1.setAttribute("class", "tagline");
+        p2.setAttribute("class", "prix");
+        article.appendChild(a);
+        a.appendChild(img);
+        a.appendChild(h2);
+        article.appendChild(p);
+        article.appendChild(p1);
+        article.appendChild(p2);
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, city, country, tagline, price, getUserCardDOM }
+}
+function photographerFactory(data) {
+    const { name, portrait, city, country, tagline, price } = data;
+
+    const picture = `assets/photographers/Photographers ID Photos/${portrait}`;
+
+    function getUserCardDOM() {
+        const article = document.createElement( 'article' );
+        const img = document.createElement( 'img' );
+        img.setAttribute("src", picture);
+        img.setAttribute("class", "portrait");
+        const h1 = document.createElement('h1');
+        h1.textContent = name;
+        const p = document.createElement( 'p' );
+        const p1 = document.createElement( 'p' );
+        p.textContent = city + ', ' + country;
+        p1.textContent = tagline;
+        p.setAttribute("class", "lieu");
+        p1.setAttribute("class", "prix");
+        article.appendChild(img);
+        article.appendChild(h1);
+        article.appendChild(p);
+        article.appendChild(p1);
+        return (article);
+    }
+    return { name, picture, city, country, tagline, price, getUserCardDOM }
+}
+function mediaFactory(data) {
+    const { title, image, likes, date, price } = data;
+
+    const picture = `assets/photographers/Mimi/${image}`;
+
+    function getMediaDOM() {
+        const section = document.createElement('section');
+        const img = document.createElement( 'img' );
+        const div = document.createElement('div');
+        const p = document.createElement('p');
+        img.setAttribute("src", picture);
+        p.textContent = title+" "+image+" "+likes+" "+date+" "+price;
+        //div.textContent = "Trier par";
+        section.appendChild(img);
+        section.appendChild(p);
+        return (section);
+    }
+    return {  picture, title, likes, date, price, getMediaDOM }
 }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,72 @@
-//Mettre le code JavaScript lié à la page photographer.html
+async function getPhotographers() {
+    // Penser à remplacer par les données récupérées dans le json
+    let photographers =[];
+    await fetch('./data/photographers.json')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("HTTP error " + response.status);
+        }
+        return response.json();
+    })
+    .then(json => {
+        console.dir(json);
+        photographers = json["photographers"];
+        console.log(photographers);
+    })
+
+    // et bien retourner le tableau photographers seulement une fois
+
+    return photographers;
+}
+async function getMedia() {
+    // Penser à remplacer par les données récupérées dans le json
+    let media =[];
+    await fetch('./data/photographers.json')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("HTTP error " + response.status);
+        }
+        return response.json();
+    })
+    .then(json => {
+        console.dir(json);
+        media = json["media"];
+        console.log(media);
+    })
+
+    // et bien retourner le tableau photographers seulement une fois
+
+    return media;
+}
+
+async function displayData(photographers, media) {
+    const photographersHeader = document.querySelector(".photograph-header");
+    const photographersMedia = document.querySelector(".photograph-media");
+    photographers.forEach((photographer) => {
+        let name = photographer['name'].replace(' ','%20');
+        if (location.search == '?'+name){
+            const photographerModel = photographerFactory(photographer);
+            const userCardDOM = photographerModel.getUserCardDOM();
+            photographersHeader.appendChild(userCardDOM);
+        } 
+        if (location.search == '?'+name){
+            media.forEach((media1) => {
+                if (media1['photographerId'] == photographer['id']){
+                    const mediaModel = mediaFactory(media1);
+                    const mediaDOM = mediaModel.getMediaDOM();
+                    photographersMedia.appendChild(mediaDOM);
+                }
+            });
+        }
+    });
+    
+};
+
+async function init() {
+    // Récupère les datas des photographes
+    const photographers = await getPhotographers();
+    const media = await getMedia();
+    displayData(photographers, media);
+};
+
+init();
